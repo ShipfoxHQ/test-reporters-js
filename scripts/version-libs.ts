1e561b365472740935bc18a3c9047ad10f642b29
Thierry ABALEA <thierry.abalea@allegoria.io>
2024-04-08 19:00:31 +0200

    Script & config to release libraries

---
+++ scripts/version-libs.ts
@@ -0,0 +1,30 @@
+import {releaseVersion} from 'nx/release';
+import yargs from 'yargs';
+
+(async () => {
+  const options = await yargs(process.argv.slice(2))
+    .version(false) // don't use the default meaning of version in yargs
+    .option('version', {
+      description: 'Explicit version specifier to use, if overriding conventional commits',
+      type: 'string',
+    })
+    .option('dryRun', {
+      alias: 'd',
+      description: 'Whether or not to perform a dry-run of the release process, defaults to true',
+      type: 'boolean',
+      default: false,
+    })
+    .option('verbose', {
+      description: 'Whether or not to enable verbose logging, defaults to false',
+      type: 'boolean',
+      default: false,
+    })
+    .parseAsync();
+
+  await releaseVersion({
+    specifier: options.version,
+    dryRun: options.dryRun,
+    verbose: options.verbose,
+  });
+  process.exit(0);
+})();
